a835a981b54b53feb066b307ddcb1236d4d0291e
Nirbhay Adhikari <fearalert1@gmail.com>
2022-07-01 14:34:01 +0545

    Profile lai jasari search implement gariyo, tesari nai Project lai ni gariyo

--- github/devsearch/projects/views.py
+++ github/devsearch/projects/views.py
@@ -2,16 +2,16 @@ import re
 from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
-from .models import Project
+from .models import Project, Tag
 from django.contrib.auth.decorators import login_required
 
 from .forms import ProjectForm
 
-
+from .utils import searchProjects
 
 def projects(request):
-    projects= Project.objects.all()
-    context={'projects':projects}
+    projects, search_query= searchProjects(request)
+    context={'projects':projects, 'search_query': search_query}
     return render(request, 'projects/projects.html',context)
 
 def project(request, pk):
