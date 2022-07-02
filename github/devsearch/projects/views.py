c4ad5a1c660d4f2ae5a124270da2a498bf997228
Nirbhay Adhikari <fearalert1@gmail.com>
2022-07-02 12:06:35 +0545

    Vote garna ra comment lekhna chai milxa, tara afnai project ma ni milxa aile lai, logged in navako le ni milxa vote hanna, yo sab chai next choti ma milaune

--- github/devsearch/projects/views.py
+++ github/devsearch/projects/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project, Tag
 from django.contrib.auth.decorators import login_required
-
-from .forms import ProjectForm
+from django.contrib import messages
+from .forms import ProjectForm, ReviewForm
 
 from .utils import searchProjects, paginateProjects
 
@@ -18,7 +18,20 @@ def projects(request):
 
 def project(request, pk):
     projectObj= Project.objects.get(id=pk)
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+
+    form=ReviewForm()
+    if request.method== 'POST':
+        form= ReviewForm(request.POST)
+        review= form.save(commit=False)
+        review.project= projectObj
+        review.owner= request.user.profile
+        review.save()
+        projectObj.getVoteCount
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+        
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'form':form})
+
 
 @login_required(login_url='login')
 def createProject(request):
