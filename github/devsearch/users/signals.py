054122a4fdb01d5317f6f2a18b6a5d39ef471a86
Nirbhay Adhikari <fearalert1@gmail.com>
2022-07-03 17:01:59 +0545

    email ko kaam vayena, tyo chode aile lai, aile api ko start gardai xu

--- github/devsearch/users/signals.py
+++ github/devsearch/users/signals.py
@@ -5,6 +5,9 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
+from django.core.mail import send_mail
+from django.conf import settings 
+
 # @receiver(post_save, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
     if created:
@@ -16,6 +19,20 @@ def createProfile(sender, instance, created, **kwargs):
         name= user.first_name
 
         )
+
+        # subject='Welcome to DevSearch'
+        # message= 'We are glad you are here'
+
+        # send_mail(
+        #     subject,
+        #     message,
+        #     settings.EMAIL_HOST_USER,
+        #     [profile.email],
+        #     fail_silently=False
+        # )
+
+
+
 def updateUser(sender, instance, created, **kwargs):
     profile=instance
     user=profile.user
